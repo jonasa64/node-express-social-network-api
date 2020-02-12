03c740d3489f1ce803d0d18c6bd5fcd1b89a3296
jonasa64 <hvidovrefifa@gmail.com>
2020-02-12 14:43:25 +0100

    set ud functions in friends crontroll

---
+++ social-network/comtrollers/friends.js
@@ -0,0 +1,31 @@
+exports.count = (req, res) => {
+    try {
+
+    }catch (e) {
+
+    }
+}
+
+exports.friendCount = (req, res) => {
+    try {
+
+    }catch (e) {
+
+    }
+}
+
+exports.remove = (req, res) => {
+    try {
+
+    }catch (e) {
+
+    }
+}
+
+exports.add = (req, res) => {
+    try {
+
+    }catch (e) {
+
+    }
+}

9fcec7dcd154bc049418cb4d2713d59720580f99
jonasa64 <hvidovrefifa@gmail.com>
2020-02-13 11:57:27 +0100

    Update app.js

--- social-network/app.js
+++ social-network/app.js
@@ -2,11 +2,28 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => {
+    console.log(`server is start on port ${port}`)
+})
+
+try {
+     mongoose.connect('',({ useNewUrlParser: true}));
+     console.log("connected to database")
+}catch (error) {
+    console.log(error);
+   console.log("failed to connect to database")
+}
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
-const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,4 +34,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+

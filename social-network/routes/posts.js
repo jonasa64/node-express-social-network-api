f5751f30ca445c1b541029abe16707db45235f76
jonasa64 <hvidovrefifa@gmail.com>
2020-02-13 15:21:46 +0100

    updated user and posts routes

--- social-network/routes/posts.js
+++ social-network/routes/posts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const  router = express.Router();
+const postCrontroller = require('../comtrollers/posts')
 //add post
-router.post('/add');
+router.post('/add', postCrontroller.creat);
 //update your post if you are the owner
 router.put('/update/:id');
 //delete your post if you are the owner
-router.delete('/destroy');
+router.delete('/destroy:id');
 //get your posts
 router.get('/:id');
 
--- social-network/routes/users.js
+++ social-network/routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../comtrollers/users');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
-router.post('/register');
+router.post('/register', userController.creat);
 router.post('/login');
 router.put('/update/:id');
 router.delete('/destroy');

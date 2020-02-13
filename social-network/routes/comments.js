dd5b9cb588478a9330dcd6448a411c843a6a3be4
jonasa64 <hvidovrefifa@gmail.com>
2020-02-13 13:02:06 +0100

    routes for comments

--- social-network/routes/comments.js
+++ social-network/routes/comments.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const  router = express.Router();
 const commentController = require('../comtrollers/comments')
-
+//add a comment
 router.post('/add', commentController.add);
-router.put('/edit:id', commentController.edit);
-router.delete('/delete', commentController.delete);
+// update a comment if you are the owner
+router.put('/update/:id', commentController.edit);
+//delete the comment if you are the owner
+router.delete('/destroy', commentController.delete);
+//get comments for a given post
+router.get('/all:id');
+
+module.exports = router;

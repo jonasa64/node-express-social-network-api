95284e1eb4601f4608d2aa960fe560aa4a6227b5
jonasa64 <hvidovrefifa@gmail.com>
2020-02-13 13:01:53 +0100

    routes for friends

--- social-network/routes/friends.js
+++ social-network/routes/friends.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const  router = express.Router();
 const friendController = require('../comtrollers/friends');
-
+// add a new friend
 router.post('/add', friendController.add);
-router.delete('/delete', friendController.remove);
-router.get('count', friendController.count);
-router.get('friend-count', friendController.friendCount);
+// remove a friend
+router.delete('/destroy', friendController.remove);
+// you friend count
+router.get('/count', friendController.count);
+//you friends count 
+router.get('/friend-count', friendController.friendCount);
+// you friends suggestions
+router.get('/suggestions');
+//viwe all you friends 
+router.get('/all/:id');
+
+module.exports = router;

0bb20903e8e1ed86ca459a32bc12f6ab8a774ca1
jonasa64 <hvidovrefifa@gmail.com>
2020-02-13 13:03:01 +0100

    Update app.js

--- social-network/app.js
+++ social-network/app.js
@@ -9,12 +9,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const dbURL = process.env.DBURL || ''
+
 app.listen(port,() => {
     console.log(`server is start on port ${port}`)
 })
 
 try {
-     mongoose.connect('',({ useNewUrlParser: true}));
+     mongoose.connect(dbURL,({ useNewUrlParser: true}));
      console.log("connected to database")
 }catch (error) {
     console.log(error);
@@ -24,6 +26,9 @@ try {
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+const friendsRoutes = require('./routes/friends');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,5 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+app.use('/friends', friendsRoutes);
 
